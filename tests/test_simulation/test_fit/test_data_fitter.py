5f6464437a3f53e5433adc669811c6b1bef7a914
Juliano Faccioni <julianofaccioni@gmail.com>
2022-03-02 21:58:50 -0300

    Added placeholder tests for fit submodule.

---
+++ tests/test_simulation/test_fit/test_data_fitter.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tests import SKIP_TESTS
+
+
+class TestFitExperimentalData(unittest.TestCase):
+    """Class representing unit-tests of the clovars.simulation.fit.data_fitter.DataFitter class."""
+
+    @unittest.skipIf(SKIP_TESTS is True, "SKIP TESTS is set to True")
+    def test_(self) -> None:
+        self.fail("Write the test!")
+
+
+if __name__ == '__main__':
+    unittest.main()
