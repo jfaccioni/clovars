ee5869accba4abd1ff8db5ac31ea0168110ddc4d
Juliano Faccioni <julianofaccioni@gmail.com>
2022-03-02 21:50:43 -0300

    Added imports to __init__.py files.

--- clovars/simulation/__init__.py
+++ clovars/simulation/__init__.py
@@ -5,7 +5,9 @@ from clovars.simulation.view.tree_drawer_2D import TreeDrawer2D
 from clovars.simulation.view.tree_drawer_3D import TreeDrawer3D
 from clovars.simulation.view.simulation_viewer import SimulationViewer
 from clovars.simulation.analysis.simulation_analyzer import SimulationAnalyzer
+from clovars.simulation.fit.data_fitter import DataFitter
 # Functions are imported later
 from clovars.simulation.run.run_simulation import run_simulation_function
 from clovars.simulation.view.view_simulation import view_simulation_function
 from clovars.simulation.analysis.analyse_simulation import analyse_simulation_function
+from clovars.simulation.fit.fit_experimental_data import fit_experimental_data_function
