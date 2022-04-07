4a98991f55f4fb52a6089bd595c26fe648d1ee7a
Juliano Faccioni <julianofaccioni@gmail.com>
2022-04-07 17:13:58 -0300

    Added run_and_view.py script to the experiments folder, for running + viewing the run results immediately after.

---
+++ scripts/experiments/signal_fluctuation_on_division/run_and_view.py
@@ -0,0 +1,15 @@
+import sys
+
+from clovars.main import main as clovars_main
+
+
+def main() -> None:
+    """Main function of this script."""
+    sys.argv = [sys.argv[0], 'run', 'run.toml', 'colonies.toml']
+    clovars_main()
+    sys.argv = [sys.argv[0], 'view', 'view.toml']
+    clovars_main()
+
+
+if __name__ == '__main__':
+    main()
