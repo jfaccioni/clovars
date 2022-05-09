4a2bcb638230b6caf1af6b222802d6cde4a11e8b
Juliano Faccioni <julianofaccioni@gmail.com>
2022-05-09 19:10:23 -0300

    Added UMAP to the tested dimension reduction models; refactored code.

--- scripts/ML Tree Test/plot_tree_stats.py
+++ scripts/ML Tree Test/plot_tree_stats.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Type
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,6 +10,7 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
+from umap import UMAP
 
 from clovars import ROOT_PATH
 
@@ -21,7 +23,8 @@ sns.set_context('paper')
 SETTINGS = {
     'input_folder': ROOT_PATH / 'scripts' / 'ML Tree Test' / 'results',
     'output_folder': ROOT_PATH / 'scripts' / 'ML Tree Test' / 'results',
-    'palette': 'rocket',
+    'save_figure': False,
+    'palette': None,
     'pca_params': {
         'n_components': 2,
     },
@@ -29,35 +32,80 @@ SETTINGS = {
         'init': 'pca',
         'learning_rate': 'auto',
     },
+    'umap_params': {
+    }
 }
+
+
+@dataclass
+class Model:
+    name: str
+    reducer: Type[PCA] | Type[TSNE] | Type[UMAP]
+    params: dict
+
+    def create(self) -> PCA | TSNE | UMAP:
+        """Returns and instance of the reducer inside the Model."""
+        return self.reducer(**self.params)
+
+    @property
+    def xlabel(self) -> str:
+        """Returns the label of the X dimension of the model."""
+        return self.name + '_1'
+
+    @property
+    def ylabel(self) -> str:
+        """Returns the label of the Y dimension of the model."""
+        return self.name + '_2'
+
+    @property
+    def labels(self) -> list[str, str]:
+        """Returns a list of the labels of the X and Y dimensions of the model."""
+        return [self.xlabel, self.ylabel]
 
 
 def main(
         input_folder: Path,
         output_folder: Path,
+        save_figure: bool = False,
         palette: str | None = None,
         pca_params: dict | None = None,
         tsne_params: dict | None = None,
+        umap_params: dict | None = None,
 ) -> None:
     """Main function of this script."""
-    pca_params = pca_params or {}
-    tsne_params = tsne_params or {}
+    # Defines the models
+    models = [
+        Model('PCA', PCA, pca_params or {}),
+        Model('t-SNE', TSNE, tsne_params or {}),
+        Model('UMAP', UMAP, umap_params or {}),
+    ]
+    hue_labels = ['treatment', 'memory']
     # Loads the data
     path = str(input_folder / 'tree_stats.csv')
     data = pd.read_csv(path, index_col=0)
     # Preprocesses the data
     labels, values = preprocess_data(data=data)
     # Plots the data
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 16))
-    hue_labels = ['treatment', 'memory_label']
+    fig, axes = plt.subplots(nrows=len(hue_labels), ncols=len(models), figsize=(24, 16))
     for ax_row, hue in zip(axes, hue_labels):
-        left_ax, right_ax = ax_row
-        plot_pca(labels=labels, values=values, hue=hue, palette=palette, pca_params=pca_params, ax=left_ax)
-        plot_tsne(labels=labels, values=values, hue=hue, palette=palette, tsne_params=tsne_params, ax=right_ax)
+        for ax, model in zip(ax_row, models):
+            fit_data = fit_to_model(model=model, labels=labels, values=values)
+            hue_order = get_hue_order(hue=hue)
+            sns.scatterplot(
+                ax=ax,
+                data=fit_data,
+                x=model.xlabel,
+                y=model.ylabel,
+                hue=hue,
+                hue_order=hue_order,
+                palette=palette,
+            )
+            ax.set_title(f'{model.name}: color by {hue}')
     plt.tight_layout()
     # Display the plots
-    path = str(output_folder / 'results.png')
-    plt.savefig(path)
+    if save_figure is True:
+        path = str(output_folder / 'results.png')
+        plt.savefig(path)
     plt.show()
 
 
@@ -72,61 +120,22 @@ def preprocess_data(data: pd.DataFrame) -> tuple[pd.DataFrame, np.ndarray]:
     return labels, values
 
 
-def plot_pca(
+def fit_to_model(
+        model: Model,
         labels: pd.DataFrame,
         values: np.ndarray,
-        hue: str,
-        palette: str,
-        pca_params: dict,
-        ax: plt.Axes = None,
-) -> plt.Axes:
-    """Plots the data in a PCA plot. Returns the Axes instance where the PCA is plotted."""
-    # Gets the Axes instance, if needed
-    if ax is None:
-        _, ax = plt.subplots()
-    # Performs PCA
-    pca = PCA(**pca_params)
-    pca_values = pca.fit_transform(values)
-    pca_results = pd.DataFrame(pca_values[:, :2], columns=['PCA1', 'PCA2'])
-    # Merges PCA data with labels
-    data = pd.concat([labels, pca_results], axis=1)
-    # Plots the data
-    hue_order = get_hue_order(hue=hue)
-    sns.scatterplot(ax=ax, data=data, x='PCA1', y='PCA2', hue=hue, hue_order=hue_order, palette=palette)
-    ax.set_title('PCA')
-    return ax
-
-
-def plot_tsne(
-        labels: pd.DataFrame,
-        values: np.ndarray,
-        hue: str,
-        palette: str,
-        tsne_params: dict,
-        ax: plt.Axes = None
-) -> plt.Axes:
-    """Plots the data in a t-SNE plot. Returns the Axes instance where the t-SNE is plotted."""
-    # Gets the Axes instance, if needed
-    if ax is None:
-        _, ax = plt.subplots()
-    # Performs t-SNE
-    tsne = TSNE(**tsne_params)
-    tsne_values = tsne.fit_transform(values)
-    tsne_results = pd.DataFrame(tsne_values[:, :2], columns=['t-SNE1', 't-SNE2'])
-    # Merges t-SNE data with labels
-    data = pd.concat([labels, tsne_results], axis=1)
-    # Plots the data
-    hue_order = get_hue_order(hue=hue)
-    sns.scatterplot(ax=ax, data=data, x='t-SNE1', y='t-SNE2', hue=hue, hue_order=hue_order, palette=palette)
-    ax.set_title('t-SNE')
-    return ax
+) -> pd.DataFrame:
+    """Fits the data to the model plot and returns it."""
+    model_values = model.create().fit_transform(values)
+    model_results = pd.DataFrame(model_values[:, :2], columns=model.labels)
+    return pd.concat([labels, model_results], axis=1)
 
 
 def get_hue_order(hue: str) -> list[str]:
     """Returns the proper hue order, given the hue string."""
     if hue == 'treatment':
         return ['Control', 'ControlTMZ', 'TMZ']
-    elif hue == 'memory_label':
+    elif hue == 'memory':
         return ['No Memory', 'Almost No Memory', 'Half Memory', 'Almost Full Memory', 'Full Memory']
     else:
         raise ValueError(f'Invalid "hue" value: "{hue}". Valid values are: "treatment", "memory_label".')
@@ -136,6 +145,7 @@ if __name__ == '__main__':
     main(
         input_folder=SETTINGS['input_folder'],
         output_folder=SETTINGS['output_folder'],
+        save_figure=SETTINGS['save_figure'],
         palette=SETTINGS['palette'],
         pca_params=SETTINGS['pca_params'],
         tsne_params=SETTINGS['tsne_params'],
