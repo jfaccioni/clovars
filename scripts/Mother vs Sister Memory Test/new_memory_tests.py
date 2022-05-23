c89d5adc074afb93bc8fe9fe33d9aa9e07e49579
Juliano Faccioni <julianofaccioni@gmail.com>
2022-05-23 17:54:32 -0300

    Inheritance test now properly uses the correlation between sisters and mother.

--- scripts/Mother vs Sister Memory Test/new_memory_tests.py
+++ scripts/Mother vs Sister Memory Test/new_memory_tests.py
@@ -9,9 +9,9 @@ from clovars.scientific import reflect_around_interval
 sns.set()
 
 SETTINGS = {
-    'mother_d1_memory': 0.15,
-    'mother_d2_memory': 0.99,
-    'sister_memory': 0.5,
+    'mother_d1_memory': 0.05,
+    'mother_d2_memory': 0.05,
+    'sister_memory': 0.78,
 }
 
 
@@ -27,28 +27,46 @@ def main(
     #     [sister_memory, 1],
     # ])
     dfs = []
+    m_mean, d1_mean, d2_mean = 0.5, 0.0, 0.0
+    m_std, d1_std, d2_std = 1.0, 1.0, 1.0
+    m_var, d1_var, d2_var = m_std**2, d1_std**2, d2_std**2
     m_d1_corr = mother_d1_memory
+    m_d1_covar = m_d1_corr * (m_std * d1_std)
     m_d2_corr = mother_d2_memory
+    m_d2_covar = m_d2_corr * (m_std * d2_std)
     d1_d2_corr = sister_memory
-    m_mean, d1_mean, d2_mean = 0.0, 0.0, 0.0
-    m_var, d1_var, d2_var = 1.0, 1.0, 1.0
-    covariance_matrix = (np.array([[d1_var, d1_d2_corr], [d1_d2_corr, d2_var]]) - (  # C_aa
-            np.array([[m_d1_corr], [m_d2_corr]]) @  # C_ab
-            np.linalg.inv(np.array([[m_var]])) @  # C_bb-1
-            np.array([[m_d1_corr, m_d2_corr]])  # C_ba
-    ))
+    d1_d2_covar = d1_d2_corr * (d1_std * d2_std)
+    # covariance_matrix = (np.array([[d1_var, d1_d2_corr], [d1_d2_corr, d2_var]]) - (  # C_aa
+    #         np.array([[m_d1_corr], [m_d2_corr]]) @  # C_ab
+    #         np.linalg.inv(np.array([[m_var]])) @  # C_bb-1
+    #         np.array([[m_d1_corr, m_d2_corr]])  # C_ba
+    # ))
+    covariance_matrix = np.array([
+        [d1_var,      d1_d2_covar],
+        [d1_d2_covar,      d2_var]
+    ]) - np.array([
+        [m_d1_covar**2,           m_d1_covar * m_d2_covar],
+        [m_d2_covar * m_d1_covar,           m_d2_covar**2]
+    ]) / m_var
     # Avoid scipy positive-semi-definite warnings (false positives):
     # Source: https://stackoverflow.com/a/41518536/11161432
     if (min_eig := np.min(np.real(np.linalg.eigvals(covariance_matrix)))) < 0:
         covariance_matrix -= 10 * min_eig * np.eye(*covariance_matrix.shape)
-    for _ in range(1000):
-        mother = np.random.uniform(low=0.0, high=1.0)
-        # mother = np.random.normal(loc=m_mean, scale=np.sqrt(m_var))
-        mean_vector = (np.array([[d1_mean], [d2_mean]]) + (  # u_a
-                np.array([[m_d1_corr], [m_d2_corr]]) @           # C_ab
-                np.linalg.inv(np.array([[m_var]])) @             # C_bb-1
-                (mother - np.array([[m_mean]]))                  # (x_b - u_b)
-        ))
+    for _ in range(10_000):
+        # mother = np.random.uniform(low=0.0, high=1.0)
+        # mean_vector = (np.array([[d1_mean], [d2_mean]]) + (      # u_a
+        #         np.array([[m_d1_corr], [m_d2_corr]]) @           # C_ab
+        #         np.linalg.inv(np.array([[m_var]])) @             # C_bb-1
+        #         (mother - np.array([[m_mean]]))                  # (x_b - u_b)
+        # ))
+        mother = np.random.normal(loc=m_mean, scale=m_std)
+        mean_vector = np.array([
+            [d1_mean],
+            [d2_mean]
+        ]) + np.array([
+            [m_d1_covar],
+            [m_d2_covar]
+        ]) * (mother - m_mean) / m_var
         fluctuation = multivariate_normal.rvs(mean=mean_vector.ravel(), cov=covariance_matrix)
         sister1 = fluctuation[0]
         sister2 = fluctuation[1]
