08a7a0418cfe10a244f4e509d7d1ab9265a50aeb
Juliano Faccioni <julianofaccioni@gmail.com>
2022-05-25 15:01:00 -0300

    Added toml files and code for producing CTOR data (new experiment).

---
+++ scripts/CTOR Analysis/produce_ctor_data.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from clovars import ROOT_PATH
+from clovars.IO import RunParameterValidator, ColonyDataFormatter
+from clovars.simulation import run_simulation_function
+
+SETTINGS = {
+    'settings_path': Path('./run.toml'),
+    'colonies_path': Path('./colonies.toml'),
+    'ctor_output_folder': ROOT_PATH / 'data' / 'ctor_analysis',
+    'curve_shifts': [-10, -5, 0, 5, 10],
+}
+
+
+def main(
+        settings_path: Path,
+        colonies_path: Path,
+        ctor_output_folder: Path,
+        curve_shifts: list[int],
+) -> None:
+    """Main function of this script."""
+    # RUN SETTINGS
+    validator = RunParameterValidator()
+    validator.parse_toml(settings_path)  # noqa
+    validator.validate()
+    params = validator.to_simulation()
+    # COLONIES SETTINGS
+    formatter = ColonyDataFormatter()
+    formatter.parse_toml(colonies_path)  # noqa
+    colony_data = formatter.to_simulation()
+    params['colony_data'] = colony_data
+    # KEEP ORIGINAL DIVISION/DEATH CURVES
+    assert len(params['colony_data']) == 1, 'Do not execute multiple colonies in this script!'
+    treatment_data = params['colony_data'][0]['treatment_data']  # only one colony
+    original_division_curves = {
+        frame: treatment['division_curve'].copy()
+        for frame, treatment in treatment_data.items()
+    }
+    original_death_curves = {
+        frame: treatment['death_curve'].copy()
+        for frame, treatment in treatment_data.items()
+    }
+    # USE CTOR OUTPUT FOLDER
+    params['simulation_writer_settings']['output_folder'] = ctor_output_folder
+    for curve_shift in curve_shifts:
+        # INJECT CTOR SETTINGS INTO PARAMS DICT
+        for curve_dict, curve_label in (
+                (original_division_curves, 'division_curve'),
+                (original_death_curves, 'death_curve'),
+        ):
+            for k in curve_dict.keys():
+                new_curve_mean = curve_dict[k]['mean'] + curve_shift
+                params['colony_data'][0]['treatment_data'][k][curve_label]['mean'] = new_curve_mean
+        # INJECT OUTPUT SETTINGS INTO PARAMS DICT
+        suffix = f"_+{curve_shift}h" if curve_shift > 0 else f"_{curve_shift}h"
+        params['simulation_writer_settings']['parameters_file_name'] = f'params{suffix}.json'
+        params['simulation_writer_settings']['cell_csv_file_name'] = f'cell_output{suffix}.csv'
+        params['simulation_writer_settings']['colony_csv_file_name'] = f'colony_output{suffix}.csv'
+        # RUN
+        run_simulation_function(**params)
+
+
+if __name__ == '__main__':
+    main(
+        settings_path=SETTINGS['settings_path'],
+        colonies_path=SETTINGS['colonies_path'],
+        curve_shifts=SETTINGS['curve_shifts'],
+        ctor_output_folder=SETTINGS['ctor_output_folder'],
+    )
---
+++ scripts/CTOR Analysis/view_ctor_data.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from clovars import ROOT_PATH
+from clovars.IO import ViewParameterValidator
+from clovars.simulation import view_simulation_function
+
+SETTINGS = {
+    'settings_path': Path('./view.toml'),
+    'ctor_output_folder': ROOT_PATH / 'data' / 'ctor_analysis',
+}
+
+
+def main(
+        settings_path: Path,
+        ctor_output_folder: Path,
+) -> None:
+    """Main function of this script."""
+    # RUN SETTINGS
+    validator = ViewParameterValidator()
+    validator.parse_toml(settings_path)  # noqa
+    validator.validate()
+    params = validator.to_simulation()
+    params['output_folder'] = str(ctor_output_folder / 'figures')
+    params['simulation_loader_settings']['simulation_input_folder'] = str(ctor_output_folder)
+    for cell_csv_file in ctor_output_folder.glob(r"cell*.csv"):
+        suffix = "_" + cell_csv_file.stem.split('_')[-1]
+        params['view_settings']['file_name_2D'] = f"CTOR{suffix}"
+        # INJECT LOADER SETTINGS INTO PARAMS DICT
+        params['simulation_loader_settings']['parameters_file_name'] = f'params{suffix}.json'
+        params['simulation_loader_settings']['cell_csv_file_name'] = f'cell_output{suffix}.csv'
+        params['simulation_loader_settings']['colony_csv_file_name'] = f'colony_output{suffix}.csv'
+        # VIEW
+        view_simulation_function(**params)
+
+
+if __name__ == '__main__':
+    main(
+        settings_path=SETTINGS['settings_path'],
+        ctor_output_folder=SETTINGS['ctor_output_folder'],
+    )
