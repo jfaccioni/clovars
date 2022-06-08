ee5cc40c4b075dda5ec528383faaea6d7c68b2ad
Juliano Faccioni <julianofaccioni@gmail.com>
2022-06-08 18:01:08 -0300

    Added new/experimental components to components.py.

--- dash_app/components.py
+++ dash_app/components.py
@@ -1,6 +1,14 @@
 from __future__ import annotations
 
+import uuid
+from typing import TYPE_CHECKING
+
+import dash
 import dash_bootstrap_components as dbc
+from dash import html, dcc, callback, Output, ALL, MATCH, Input
+
+if TYPE_CHECKING:
+    from dash.development.base_component import Component
 
 
 def NumericInputGroup(
@@ -44,3 +52,62 @@ def NumericInputGroup(
     else:
         prefix = dbc.InputGroupText(children=prefix) if prefix else None
     return dbc.InputGroup([prefix, inputbox, suffix])
+
+
+def CollapsableDiv(
+        children: list[Component] | None = None,
+        name: str = "",
+        label: str = "",
+        checked: bool = False,
+) -> html.Div:
+    """Returns a html div containing a checkbox that opens/closes a collapsable component."""
+    name = name or str(uuid.uuid4())
+    checkbox_id = {'type': 'collapsable-div-checkbox', 'name': name}
+    collapse_id = {'type': 'collapsable-div-collapse', 'name': name}
+    return html.Div([
+        dbc.Checkbox(id=checkbox_id, label=label or None, value=checked),
+        dbc.Collapse(id=collapse_id, children=children or [], is_open=checked)
+    ])
+
+
+def DivSelectorDropdown(
+        children: dict[str, Component] | None = None,
+        name: str = "",
+) -> html.Div:
+    """Returns a html div containing a selector that displays a given div in the children dictionary."""
+    if children is None:
+        return html.Div([])
+    name = name or str(uuid.uuid4())
+    choices = []
+    elements = []
+    dropdown_id = {'type': 'div-selector-dropdown', 'name': name}
+    for index, (key, value) in enumerate(children.items()):
+        choices.append(key)
+        child_id = {'type': 'div-selector-child', 'label': key, 'index': index, 'name': name}
+        value.id = child_id
+        elements.append(value)
+    options = [
+        {'label': choice, 'value': index}
+        for index, choice in enumerate(choices)
+    ]
+    return html.Div([dcc.Dropdown(id=dropdown_id, options=options), *elements])
+
+
+@callback(
+    Output({'type': 'div-selector-child', 'parent-label': ALL, 'name': MATCH}, 'style'),
+    Input({'type': 'div-selector-dropdown', 'name': MATCH}, 'value'),
+)
+def change_display_widget(dropdown_index: int | None) -> list[dict[str, str]]:
+    values = [{'display': 'none'}] * len(dash.callback_context.outputs_list)
+    if dropdown_index is not None:
+        values[dropdown_index] = {'display': 'inline'}
+    return values
+    # outputs = dash.callback_context.outputs_list
+    # if not dropdown_value:
+    #     return [{'display': 'none'}] * len(outputs)
+    # return [
+    #     {'display': 'inline'}
+    #     if output['id']['parent'] == dropdown_value
+    #     else {'display': 'none'}
+    #     for output in dash.callback_context.outputs_list
+    # ]
