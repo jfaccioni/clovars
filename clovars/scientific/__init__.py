1b06ae894af3886e1215f498a131ac70dfb0ee1f
Juliano Faccioni <julianofaccioni@gmail.com>
2022-07-29 15:39:32 -0300

    Updated imports from scientific package.

--- clovars/scientific/__init__.py
+++ clovars/scientific/__init__.py
@@ -1,15 +1,6 @@
 from clovars.scientific.sctypes import Curve, Numeric
 from clovars.scientific.utils import reflect_around_interval, triangular_wave
 from clovars.scientific.brownian_motion import brownian_motion, bounded_brownian_motion
-from clovars.scientific.cell_signal import (
-    CellSignal,
-    ConstantCellSignal,
-    EMGaussianCellSignal,
-    GaussianCellSignal,
-    SinusoidalCellSignal,
-    StochasticCellSignal,
-    StochasticSinusoidalCellSignal,
-    get_cell_signal,
-)
 from clovars.scientific.curves import AbstractCurve, EMGaussian, Gamma, Gaussian, Lognormal, get_curve
 from clovars.scientific.distribution import Distribution, Oscillator, get_distribution, get_oscillator, get_wave
+from clovars.scientific.cell_signal import CellSignal, get_cell_signal
