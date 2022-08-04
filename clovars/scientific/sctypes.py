9bd19c1ea26004f005587901eb040b33d9364644
Juliano Faccioni <julianofaccioni@gmail.com>
2022-08-04 16:04:19 -0300

    Updated types in sctypes.py.

--- clovars/scientific/sctypes.py
+++ clovars/scientific/sctypes.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 import numpy as np
 from scipy.stats import norm, exponnorm, gamma, lognorm
 
-Numeric = int | float | np.ndarray
-Curve = type[norm] | type[exponnorm] | type[gamma] | type[lognorm]
+Numeric = int | float | np.ndarray  # noqa
+Curve = type[norm] | type[exponnorm] | type[gamma] | type[lognorm]  # noqa
