8e8afb7c8898688b423ab19efc1a499aa220f51d
Juliano Faccioni <julianofaccioni@gmail.com>
2022-08-04 16:10:55 -0300

    Updated imports.

--- clovars/abstract/__init__.py
+++ clovars/abstract/__init__.py
@@ -1,3 +1,3 @@
-from clovars.abstract.cell_node import CellNode
-from clovars.abstract.circle import Circle
-from clovars.abstract.memory import CellMemory
+from .cell_node import CellNode
+from .circle import Circle
+from .memory import CellMemory
--- clovars/scientific/__init__.py
+++ clovars/scientific/__init__.py
@@ -1,6 +1,6 @@
-from clovars.scientific.sctypes import Curve, Numeric
-from clovars.scientific.utils import reflect_around_interval, triangular_wave
-from clovars.scientific.brownian_motion import brownian_motion, bounded_brownian_motion
-from clovars.scientific.curves import AbstractCurve, EMGaussian, Gamma, Gaussian, Lognormal, get_curve
-from clovars.scientific.distribution import Distribution, Oscillator, get_distribution, get_oscillator, get_wave
-from clovars.scientific.cell_signal import CellSignal, get_cell_signal
+from .sctypes import Curve, Numeric
+from .utils import get_correlated_values, reflect_around_interval, triangular_wave
+from .brownian_motion import brownian_motion, bounded_brownian_motion
+from .curves import AbstractCurve, EMGaussian, Gamma, Gaussian, Lognormal, get_curve
+from .distribution import Distribution, Oscillator, get_distribution, get_oscillator, get_wave
+from .cell_signal import CellSignal, get_cell_signal
