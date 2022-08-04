0733999c6ca3efb239ae4647bd6f87d4c8f83c03
Juliano Faccioni <julianofaccioni@gmail.com>
2022-08-04 16:06:58 -0300

    Added get_correlated_values function to scientific/utils.py.

--- clovars/scientific/utils.py
+++ clovars/scientific/utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+
+
 def reflect_around_interval(
         x: float,
         lower_bound: float,
@@ -17,3 +21,45 @@ def triangular_wave(x, period, amplitude):
     Equation source: https://en.wikipedia.org/wiki/Triangle_wave
     """
     return (4*amplitude/period) * abs(((x - period/4) % period) - period/2) - amplitude
+
+
+def get_correlated_values(
+        x: float,
+        x_mean: float,
+        x_std: float,
+        y_mean: float,
+        y_std: float,
+        z_mean: float,
+        z_std: float,
+        x_y_corr: float,
+        x_z_corr: float,
+        y_z_corr: float,
+) -> tuple[float, float]:
+    """
+    Given a value x and three correlation values, returns two new values (y and z)
+    that have a specific correlation among themselves.
+    """
+    # SOURCE: https://stats.stackexchange.com/a/437682/325570
+    x_var, y_var, z_var = x_std**2, y_std**2, z_std**2
+    x_y_covar = x_y_corr * (x_std * y_std)
+    x_z_covar = x_z_corr * (x_std * z_std)
+    y_z_covar = y_z_corr * (y_std * z_std)
+    covariance_matrix = np.array([
+        [y_var,     y_z_covar],
+        [y_z_covar,     z_var]
+    ]) - np.array([
+        [x_y_covar**2,          x_y_covar * x_z_covar],
+        [x_z_covar * x_y_covar,          x_z_covar**2]
+    ]) / x_var
+    # Avoid scipy positive-semi-definite warnings (false positives):
+    # Source: https://stackoverflow.com/a/41518536/11161432
+    if (min_eig := np.min(np.real(np.linalg.eigvals(covariance_matrix)))) < 0:
+        covariance_matrix -= 10 * min_eig * np.eye(*covariance_matrix.shape)
+    mean_vector = np.array([
+        [y_mean],
+        [z_mean]
+    ]) + np.array([
+        [x_y_covar],
+        [x_z_covar]
+    ]) * (x - x_mean) / x_var
+    return multivariate_normal.rvs(mean=mean_vector.ravel(), cov=covariance_matrix)
