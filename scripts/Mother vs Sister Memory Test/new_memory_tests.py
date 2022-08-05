df74618090efb201e979c608bc03e580c1e158a8
Juliano Faccioni <julianofaccioni@gmail.com>
2022-08-05 16:02:17 -0300

    Trying new stuff in new_memory_tests script - searching for the logical error.

--- scripts/Mother vs Sister Memory Test/new_memory_tests.py
+++ scripts/Mother vs Sister Memory Test/new_memory_tests.py
@@ -7,9 +7,9 @@ from scipy.stats import multivariate_normal
 sns.set()
 
 SETTINGS = {
-    'mother_d1_memory': 0.15,
-    'mother_d2_memory': 0.85,
-    'sister_memory': 0.66,
+    'mother_d1_memory': 0.56,
+    'mother_d2_memory': 0.98,
+    'sister_memory': 0.15,
 }
 
 
@@ -25,8 +25,8 @@ def main(
     #     [sister_memory, 1],
     # ])
     dfs = []
-    m_mean, d1_mean, d2_mean = 0.5, 0.0, 0.0
-    m_std, d1_std, d2_std = 1.0, 1.0, 1.0
+    m_mean, d1_mean, d2_mean = 0.0, 0.0, 0.0
+    m_std, d1_std, d2_std = 5.0, 5.0, 5.0
     m_var, d1_var, d2_var = m_std**2, d1_std**2, d2_std**2
     m_d1_corr = mother_d1_memory
     m_d1_covar = m_d1_corr * (m_std * d1_std)
@@ -57,45 +57,21 @@ def main(
         #         np.linalg.inv(np.array([[m_var]])) @             # C_bb-1
         #         (mother - np.array([[m_mean]]))                  # (x_b - u_b)
         # ))
-        mother = np.random.normal(loc=m_mean, scale=m_std)
+        mother = np.random.normal(loc=0.0, scale=5.0)
         mean_vector = np.array([
             [d1_mean],
             [d2_mean]
         ]) + np.array([
             [m_d1_covar],
             [m_d2_covar]
-        ]) * (mother - m_mean) / m_var
+        ]) * (m_mean - m_mean) / m_var
         fluctuation = multivariate_normal.rvs(mean=mean_vector.ravel(), cov=covariance_matrix)
         sister1 = fluctuation[0]
         sister2 = fluctuation[1]
-        # sister1 = mother + fluctuation[0]
-        # sister2 = mother + fluctuation[1]
-        # sister1 = logistic.cdf(mother + fluctuation[0] * (1-mother_memory), loc=0.5)
-        # sister2 = logistic.cdf(mother + fluctuation[1] * (1-mother_memory), loc=0.5)
-        # sister1 = reflect_around_interval(
-        #     x=fluctuation[0],
-        #     lower_bound=0.0,
-        #     upper_bound=1.0,
-        # )
-        # sister2 = reflect_around_interval(
-        #     x=fluctuation[1],
-        #     lower_bound=0.0,
-        #     upper_bound=1.0,
-        # )
-        # sister1 = reflect_around_interval(
-        #     x=mother + fluctuation[0],
-        #     lower_bound=0.0,
-        #     upper_bound=1.0,
-        # )
-        # sister2 = reflect_around_interval(
-        #     x=mother + fluctuation[1],
-        #     lower_bound=0.0,
-        #     upper_bound=1.0,
-        # )
         df = pd.DataFrame({
             'Mother': mother,
-            'Sister1': sister1,
-            'Sister2': sister2,
+            'Sister1': sister1+50,
+            'Sister2': sister2+50,
         }, index=[0])
         dfs.append(df)
     data = pd.concat(dfs, ignore_index=True)
