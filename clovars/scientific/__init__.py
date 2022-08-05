87a6ec0563ac5d444c0454f2b06049b1d3c5653a
Juliano Faccioni <julianofaccioni@gmail.com>
2022-08-05 10:18:55 -0300

    Changed imports.

--- clovars/scientific/__init__.py
+++ clovars/scientific/__init__.py
@@ -2,5 +2,13 @@ from .sctypes import Curve, Numeric
 from .utils import get_correlated_values, reflect_around_interval, triangular_wave
 from .brownian_motion import brownian_motion, bounded_brownian_motion
 from .curves import AbstractCurve, EMGaussian, Gamma, Gaussian, Lognormal, get_curve
-from .distribution import Distribution, MultivariateGaussianDistribution, Oscillator, Wave, get_distribution, get_oscillator, get_wave
+from .distribution import (
+    Distribution,
+    MultivariateDistribution,
+    Oscillator,
+    Wave,
+    get_distribution,
+    get_oscillator,
+    get_wave,
+)
 from .cell_signal import CellSignal, get_cell_signal
